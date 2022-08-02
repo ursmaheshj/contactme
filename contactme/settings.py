05ab11d86baf190ce548de481ddae6e74ced525e
Omkar Jadhav <omkar28399@gmail.com>
2022-08-03 01:26:45 +0530

    debug changed to false

--- contactme/settings.py
+++ contactme/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kez2&l@(ujy_rb4*eku9cj)&e8-wtx9^qd0%wha+a5e%q%^cz9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['contactme2899.herokuapp.com']
 
